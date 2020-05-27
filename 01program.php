f0f20ace8991df7e5aa7ac0a01f26f802affe92c
dikejbang <66010247+dikejbang@users.noreply.github.com>
2020-05-27 12:29:38 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -1,7 +1,10 @@
 <?php
 echo ("Witaj")
 echo ("<br> Jestem programem 01");
-echo ("<br> Test");
+echo ("<br> Test XD");
+echo ("<br> TEST 11111");
+echo ("<br> Dalsza część programu");
+echo ("<br> Dalsza część programu edytowana przez github.com");
 ?>
 
 
